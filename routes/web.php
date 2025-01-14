992c807b6fbd024776449d947415aa0dc90b4a6c
evanosouth <evanosouth@gmail.com>
2025-01-14 02:01:05 +0000

    pertemuan 15

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\dashboardController;
+use App\Http\Controllers\pegawaiController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -16,6 +17,61 @@ Route::middleware(['auth', 'cekLevel:superadmin,admin'])->group(function(){
     Route::get('/dashboard', [dashboardController::class, 'index']);
 
     Route::get('/logout', [AuthController::class, 'logout']);
+
+
+    /**
+     * Ini routing pegawai
+     */
+
+    Route::controller(pegawaiController::class)->group(function(){
+
+        Route::get('/pegawai', 'index');
+
+        Route::get('/pegawai/add', 'store')->name('addPegawai');
+
+
+    });
+
+
+
+
+
+    /**
+     * Ini routing Stok
+     */
+
+
+
+
+
+
+    /**
+     * Ini routing Barang Masuk
+     */
+
+
+
+
+
+    /**
+     * Ini routing Barang Keluar
+     */
+
+
+
+
+
+
+    /**
+     * Ini routing Pelanggan
+     */
+
+
+
+
+    /**
+     * Ini routing Suplier
+     */
 
 
 });
